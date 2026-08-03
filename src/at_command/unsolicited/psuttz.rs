12fd37c1489ca9063f7fb9551ee963688728495d
Ellie Hayward <ellie@witchymail.com>
2026-08-03 15:55:11 +0100

    Added receive time instant to parsed psuttz struct

--- src/at_command/unsolicited/psuttz.rs
+++ src/at_command/unsolicited/psuttz.rs
@@ -1,23 +1,34 @@
 use crate::at_command::{AtParseErr, AtParseLine, cclk};
 
-#[derive(Clone, Copy, Default, Debug)]
+#[derive(Clone, Copy, Debug)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub struct Psuttz {
     pub utc_time: cclk::types::UtcDateTime,
     pub tz_offset: Option<cclk::types::LocalTimeOffset>,
+    pub instant: embassy_time::Instant,
+}
+
+impl Default for Psuttz {
+    fn default() -> Self {
+        Self {
+            utc_time: Default::default(),
+            tz_offset: None,
+            instant: embassy_time::Instant::MIN,
+        }
+    }
 }
 
 impl AtParseLine for Psuttz {
-    fn from_line(line: &str, _instant: &embassy_time::Instant) -> Result<Self, AtParseErr> {
+    fn from_line(line: &str, instant: &embassy_time::Instant) -> Result<Self, AtParseErr> {
         let line = line
             .strip_prefix("*PSUTTZ:")
             .ok_or(AtParseErr::Mismatch)?
             .trim();
-        let (utc_time, tz_offset) =
-            cclk::parse_psuttz_time(line)?;
+        let (utc_time, tz_offset) = cclk::parse_psuttz_time(line)?;
         Ok(Self {
             utc_time,
             tz_offset,
+            instant: *instant,
         })
     }
 }
